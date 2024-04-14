e414ef1477d10c32d851d158757aeebebde7858e
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-04-14 15:46:55 -0500

    Complete all required features

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,22 +1,43 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// Declare basket variable
 let basket = [];
 
+// Function to add item to basket
 function addItem(item) {
-// Add new item to basket
-// Return true when item is added
+basket.push(item);
+return true;
 }
 
-function listItems() {
-// Loop over items in basket array
-// Console.log each item on new line
+// Test addItem function. Returns true.
+console.log(addItem('Socks'));
+console.log(addItem('New shoes'));
+console.log(`Items in your basket: ${basket}`);
+
+// Function to log each item in the basket
+function listItems(item) {
+for (item of basket) {
+    console.log(item);
+}
 }
 
+// Test listItems function
+listItems()
+
+// Function to empty basket
 function empty() {
-// Reset basket to empty
+while (basket.length > 0) {
+    basket.pop()
+}
 }
 
+// Test empty function
+empty();
+console.log(basket)
+
+// STRETCH GOALS:
 
 
 
